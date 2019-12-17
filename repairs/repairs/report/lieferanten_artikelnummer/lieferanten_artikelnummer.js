3291863ff8b72f29460d354f1550429f63c52fc0
Lyefyre <v.schaer@gmx.ch>
2019-12-17 15:24:56 +0100

    report

--- repairs/repairs/report/lieferanten_artikelnummer/lieferanten_artikelnummer.js
+++ repairs/repairs/report/lieferanten_artikelnummer/lieferanten_artikelnummer.js
@@ -1,9 +1,9 @@
-// Copyright (c) 2016, libracore AG and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
 frappe.query_reports["Lieferanten-Artikelnummer"] = {
-	"filters": [
-
-	]
-};
+    "filters": [
+        {
+            "fieldname":"item",
+            "label": __("Item Code"),
+            "fieldtype": "Link"
+	}
+    ]
+}
--- repairs/repairs/report/lieferanten_artikelnummer/lieferanten_artikelnummer.py
+++ repairs/repairs/report/lieferanten_artikelnummer/lieferanten_artikelnummer.py
@@ -2,8 +2,18 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 
 def execute(filters=None):
 	columns, data = [], []
-	return columns, data
+	columns = ["Item Code:Link/Item:200"]
+
+	if filters.item:
+		item = filters.item
+	else:
+		item = "%"
+
+	sql_query = "SELECT name FROM `tabItem`".format(item=item)
+
+	data = frappe.db.sql(sql_query, as_list = True)
+	#return columns, data
